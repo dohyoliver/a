2a2f30b145a58e0952fbb8aa15b5ff187c99a004
dohyoliver <dohyoliver@gmail.com>
2021-05-31 19:34:01 +0200

    haha

--- Mappa/style/script.js
+++
@@ -1,7 +0,0 @@
-function gallery(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
-  expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
-  expandImg.parentElement.style.display = "block";
-}
